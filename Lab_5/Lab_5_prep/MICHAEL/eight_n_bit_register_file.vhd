129660024e73b764ccf5370cf98f45c3b3b7945a
MicSel <micsel@outlook.com>
2015-11-24 12:57:49 +0000

    Michael_8BitRegisterFile_and_tdm_display_circuit_v1
    First commit

---
+++ Lab_5/Lab_5_prep/MICHAEL/eight_n_bit_register_file.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    12:55:41 11/24/2015 
+-- Design Name: 
+-- Module Name:    eight_n_bit_register_file - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity eight_n_bit_register_file is
+end eight_n_bit_register_file;
+
+architecture Behavioral of eight_n_bit_register_file is
+
+begin
+
+
+end Behavioral;
+
---
+++ Lab_5/Lab_5_prep/MICHAEL/tdm_display_circuit.vhd
@@ -0,0 +1,32 @@
+----------------------------------------------------------------------------------
+-- Company: 	QMUL
+-- Engineer: 	Michael Seltene
+-- 
+-- Create Date:    12:55:57 11/24/2015 
+-- Design Name: 	 tdm_display_circuit
+-- Module Name:    tdm_display_circuit - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity tdm_display_circuit is
+end tdm_display_circuit;
+
+architecture Behavioral of tdm_display_circuit is
+
+begin
+
+
+end Behavioral;
+
